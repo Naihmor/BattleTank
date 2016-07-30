0740531d54257dfcdc8349982e23c67b63f3c952
Ray Naihmor <Ray Naihmor>
2016-07-30 20:06:33 +0200

    BT14: Loggin out PlayerController Tank possession

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -3,7 +3,21 @@
 #include "BattleTank.h"
 #include "TankPlayerController.h"
 
-ATank ATankPlayerController::GetControlledTank() const
+void ATankPlayerController::BeginPlay()
 {
-	return Cast<ATank>GetPawn();
+	Super::BeginPlay();
+	auto Controll = GetControlledTank();
+	if (Controll)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("%s Begins Play"), *(Controll->GetName()));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController not possessing a Tank"));
+	}
+}
+
+ATank* ATankPlayerController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
 }
