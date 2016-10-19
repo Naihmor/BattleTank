a13fa5ecf5b3eb9a0b01c363da464c6a692457f6
Javier <naihmor@gmail.com>
2016-10-19 21:51:44 +0200

    BT22: The TankBarrel now can elevates between a max and min position, at a max speed. It elevates automatically if it can reach an obj with the simulated proyectile. TODO: fix the weird bounding of the tanks from the scene.

--- BattleTank/Source/BattleTank/Public/TankBarrel.h
+++ BattleTank/Source/BattleTank/Public/TankBarrel.h
@@ -19,7 +19,7 @@ public:
 	
 private:
 	UPROPERTY(EditAnywhere, Category = Setup)
-	float MaxDegreedPerSecond = 20;
+	float MaxDegreesPerSecond = 5;
 
 	UPROPERTY(EditAnywhere, Category = Setup)
 	float MaxElevationDegrees = 40;
